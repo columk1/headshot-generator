371ae7bb876a25a779719fa908e1dc0c2e6a9e62
Colum Kelly <columk1@users.noreply.github.com>
2025-10-05 23:07:52 -0700

    feat: create initial migration

---
+++ lib/db/migrations/0000_bitter_luckman.sql
@@ -0,0 +1,61 @@
+CREATE TABLE `users` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`email` text NOT NULL,
+	`password_hash` text NOT NULL,
+	`stripe_customer_id` text,
+	`created_at` integer DEFAULT (cast(unixepoch() as int)) NOT NULL,
+	`updated_at` integer,
+	`deleted_at` integer
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `users_stripe_customer_id_unique` ON `users` (`stripe_customer_id`);--> statement-breakpoint
+CREATE UNIQUE INDEX `users_email_unique` ON `users` (`email`);--> statement-breakpoint
+CREATE TABLE `activity_logs` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`user_id` integer,
+	`action` text NOT NULL,
+	`timestamp` integer DEFAULT (cast(unixepoch() as int)) NOT NULL,
+	`ip_address` text,
+	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `reference_images` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`session_id` integer NOT NULL,
+	`url` text NOT NULL,
+	`created_at` integer DEFAULT (cast(unixepoch() as int)) NOT NULL,
+	FOREIGN KEY (`session_id`) REFERENCES `training_sessions`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `generations` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`user_id` integer NOT NULL,
+	`gender` text NOT NULL,
+	`background` text NOT NULL,
+	`input_image_url` text NOT NULL,
+	`status` text DEFAULT 'PENDING_PAYMENT' NOT NULL,
+	`image_url` text,
+	`created_at` integer DEFAULT (cast(unixepoch() as int)) NOT NULL,
+	`retry_count` integer DEFAULT 0 NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `orders` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`user_id` integer NOT NULL,
+	`generation_id` integer NOT NULL,
+	`stripe_payment_intent_id` text NOT NULL,
+	`amount_paid` integer NOT NULL,
+	`status` text DEFAULT 'pending' NOT NULL,
+	`created_at` integer DEFAULT (cast(unixepoch() as int)) NOT NULL,
+	`updated_at` integer,
+	FOREIGN KEY (`generation_id`) REFERENCES `generations`(`id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `training_sessions` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`user_id` integer NOT NULL,
+	`created_at` integer DEFAULT (cast(unixepoch() as int)) NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
+);
